aef75b9c672c58db7cc6f0996b26aea3559c55c7
Adolfo R. Brandes <arbrandes@arbrand.es>
2023-11-02 16:50:52 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { createConfig } = require('@edx/frontend-build');
+
+module.exports = createConfig('eslint');
---
+++ src/example/ExamplePage.jsx
@@ -0,0 +1,12 @@
+import { Container } from '@edx/paragon';
+
+const ExamplePage = () => (
+  <main>
+    <Container className="py-5">
+      <h1>Example Page</h1>
+      <p>Hello world!</p>
+    </Container>
+  </main>
+);
+
+export default ExamplePage;
