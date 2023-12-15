af931ed7a9428b6442ef605c9cafabd971d09f39
Jason Wesson <jwesson@2u.com>
2023-12-15 02:04:37 +0000

    feat: WIP add file structure and write mocks for tests

---
+++ src/plugins/directPlugins/test/DirectPluginSlot.test.jsx
@@ -0,0 +1,4 @@
+// when no additional plugins, only render default
+// when no defaults, only plugin is rendered
+// when no plugins and no defaults, nothing is rendered
+// when more than one plugin, priority is followed
