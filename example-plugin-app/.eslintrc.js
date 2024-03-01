577206bef7071c49e0553d120a4cddc8947e9956
Jason Wesson <jwesson@2u.com>
2024-03-01 01:27:02 +0000

    fix: remove frontend-platform from example app package.json
    * fix lint in example apps

--- example-plugin-app/.eslintrc.js
+++ example-plugin-app/.eslintrc.js
@@ -1,10 +1,11 @@
 const { getBaseConfig } = require('@openedx/frontend-build');
+const path = require('path');
 
 const config = getBaseConfig('eslint');
 
 config.rules = {
   'import/no-extraneous-dependencies': ['error', {
-    devDependencies: true,
+    packageDir: path.join(__dirname, '..'),
   }],
   'import/no-unresolved': ['error', {
     ignore: ['@edx/frontend-plugin-framework*'],
